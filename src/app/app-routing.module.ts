100da8b12b09ac190aa7b00fcf22fdb5b4b1db23
JuniorPita <81571083+JuniorPita@users.noreply.github.com>
2022-04-11 17:12:24 +0300

    Added the basic files with page-routing to our project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CardPageComponent} from './card-page/card-page.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {ProductPageComponent} from './product-page/product-page.component';
+import {MainLayoutComponent} from './shared/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/',
+        pathMatch: 'full'
+      },
+
+      {
+        path: '',
+        component: MainPageComponent
+      },
+
+      {
+        path: 'product/:id',
+        component: ProductPageComponent
+      },
+
+      {
+        path: 'card',
+        component: CardPageComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CardPageComponent } from './card-page/card-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    MainPageComponent,
+    ProductPageComponent,
+    CardPageComponent
   ],
   imports: [
     BrowserModule,
